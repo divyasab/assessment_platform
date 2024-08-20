6301522ab64a6c28784ad2a8a2d0b8c63d98cbea
divya sable <divyasable21@navgurukul.org>
2024-08-20 12:24:10 +0530

    Footer is done

---
+++ app/Component/Footer.js
@@ -0,0 +1,19 @@
+const Footer=()=>{
+    return(
+        <>
+            <div className="footer-content">
+                <div className="main-row">
+                    {/* <div className="footer-text"> */}
+                    <div className="footer-text"> Footer</div>
+                        <div className="footer-headline">
+                            © Copyright, All Rights Reserved by Ungineering
+                        </div>
+                    {/* </div>   */}
+                </div>
+            </div>
+        </>
+    )
+
+    
+}
+export default Footer
--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Footer from "./Component/Footer";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -14,9 +15,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>{children}
       <link rel="stylesheet"href="https://fonts.googleapis.com/css?family=Inria Sans"></link>
-        {/* <div className="main-container">  
           <Footer/> 
-        </div>  */}
       </body>
     </html>
   );
